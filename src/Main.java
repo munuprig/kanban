3033605bafb272ff796e9be1b40d0498c0fa66fc
Egor <munuprig@gmail.com>
2025-05-12 17:12:10 +0300

    Спринт 6 первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import data.ProgressTask;
-import interfaces.HistoryManager;
 import interfaces.TaskManager;
 import manager.Managers;
 import models.Epic;
@@ -69,23 +67,22 @@ public class Main {
     }
 
     private static void printAllTasks(TaskManager manager) {
-//        System.out.println("Задачи:");
-//        for (Task task : manager.getTasks()) {
-//            System.out.println(task);
-//        }
-//        System.out.println("Эпики:");
-//        for (Task epic : manager.getEpics()) {
-//            System.out.println(epic);
-//
-//            for (Task task : manager.getEpicSubTasks(epic.getId())) {
-//                System.out.println("--> " + task);
-//            }
-//        }
-//        System.out.println("Подзадачи:");
-//        for (Task subtask : manager.getSubTasks()) {
-//            System.out.println(subtask);
-//        }
+        System.out.println("Задачи:");
+        for (Task task : manager.getTasks()) {
+            System.out.println(task);
+        }
+        System.out.println("Эпики:");
+        for (Task epic : manager.getEpics()) {
+            System.out.println(epic);
 
+            for (Task task : manager.getEpicSubTasks(epic.getId())) {
+                System.out.println("--> " + task);
+            }
+        }
+        System.out.println("Подзадачи:");
+        for (Task subtask : manager.getSubTasks()) {
+            System.out.println(subtask);
+        }
         System.out.println("История:");
         for (Task task : manager.getHistory()) {
             System.out.println(task);
